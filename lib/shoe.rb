b668e7e8190b2ed63fa99a66e3c2655bf63867da
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 23:54:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+
+class shoe
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor:brand
+  attr_accessor:color
+  attr_accessor:size
+  attr_accessor:material
+  attr_accessor:condition
+
+  def cobble
+    puts "The shoe has been repaired"
+    brand.shoe.condition("new")
+  end
+
+end
