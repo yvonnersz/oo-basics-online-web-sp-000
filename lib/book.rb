a996d71e5900f6775e52c64000a4ca4ed4ceef46
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 23:57:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 
 class Book
 
-  attr_accessor:title, :author, :page_count, :genre
+  attr_accessor:, :author, :page_count, :genre
+  attr_reader: title
 
   def initialize(title)
     @title= title
