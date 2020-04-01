13ebd73695bbe4306658e3a3495d22e519699bd8
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 23:56:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 
 class Book
 
+  attr_accessor:title, :author, :page_count, :genre
+
+
   def initialize(title)
     @title= title
   end
 
-  attr_accessor:title, :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
